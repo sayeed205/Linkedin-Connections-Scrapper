64a6e6109d819bbe26fd630c779d843b7869b50f
Hitarashi <sayeed205@gmail.com>
2023-03-27 20:00:00 +0530

    Show connection count in extension
    Shows connections count in extension popup
    linkedin logo redirects to connection page

--- src/contentScript.ts
+++ src/contentScript.ts
@@ -1,14 +1,107 @@
 "use strict";
+// import axios from "axios";
 
-// Content script file will run in the context of web page.
-// With content script you can manipulate the web pages using
-// Document Object Model (DOM).
-// You can also pass information to the parent extension.
+// // Content script file will run in the context of web page.
+// // With content script you can manipulate the web pages using
+// // Document Object Model (DOM).
+// // You can also pass information to the parent extension.
 
-// We execute this script by making an entry in manifest.json file
-// under `content_scripts` property
+// // We execute this script by making an entry in manifest.json file
+// // under `content_scripts` property
 
-// For more information on Content Scripts,
-// See https://developer.chrome.com/extensions/content_scripts
+// // For more information on Content Scripts,
+// // See https://developer.chrome.com/extensions/content_scripts
 
-// todo)) add logic
+// const handleXHR = (
+//   type: "GET" | "POST",
+//   url: string,
+//   data: FormData | null,
+//   cb: XHRcb
+// ) => {
+//   const xhr = new XMLHttpRequest();
+//   xhr.open(type, url, true);
+//   xhr.withCredentials = true;
+//   xhr.setRequestHeader(
+//     "accept",
+//     "application/vnd.linkedin.normalized+json+2.1"
+//   );
+//   xhr.setRequestHeader("csrf-token", CSRF_TOKEN);
+//   xhr.setRequestHeader("x-li-lang", "en_US");
+//   xhr.setRequestHeader(
+//     "x-li-track",
+//     '{"clientVersion":"1.2.3179","osName":"web","timezoneOffset":-7,"deviceFormFactor":"DESKTOP","mpName":"voyager-web"}'
+//   );
+//   xhr.setRequestHeader("x-restli-protocol-version", "2.0.0");
+//   xhr.send(data);
+//   xhr.onreadystatechange = function () {
+//     if (xhr.readyState === 4) {
+//       cb(JSON.parse(this.response));
+//     }
+//   };
+// };
+
+import axios from "axios";
+
+// Get CSRF token from cookie
+const COOKIE = document.cookie;
+const match = COOKIE.match(/JSESSIONID="(.*)"/);
+const CSRF_TOKEN = match ? match[1].split('"')[0] : "";
+
+interface ConnectionCount {
+  htmlCount: number;
+  trueCount: number;
+}
+
+// Get connection count
+const getConnectionCount = async (): Promise<ConnectionCount> => {
+  // Get HTML node containing connection count
+  const [HTMLnode] = document.getElementsByClassName(
+    "mn-connections__header"
+  ) as HTMLCollectionOf<HTMLElement>;
+
+  let htmlCount = 0;
+  let trueCount = 0;
+
+  if (HTMLnode) {
+    // Parse HTML node text to get connection count
+    htmlCount =
+      parseInt(HTMLnode.innerText.split(" ")[0].replace(/,/g, "")) || 0;
+  }
+
+  // Make API call to get actual number of connections
+  const url = `https://www.linkedin.com/voyager/api/relationships/connections?count=${htmlCount}&start=0`;
+
+  const { data } = await axios.get(url, {
+    headers: {
+      accept: "application/vnd.linkedin.normalized+json+2.1",
+      "csrf-token": CSRF_TOKEN,
+      "x-li-lang": "en_US",
+      "x-li-track":
+        '{"clientVersion":"1.2.3179","osName":"web","timezoneOffset":-7,"deviceFormFactor":"DESKTOP","mpName":"voyager-web"}',
+      "x-restli-protocol-version": "2.0.0",
+    },
+    withCredentials: true,
+  });
+
+  if (data.included.length > 0) {
+    const connections = data.included.filter(
+      (el: { $type: string }) =>
+        el.$type === "com.linkedin.voyager.identity.shared.MiniProfile"
+    );
+    trueCount = connections.map(
+      (el: { publicIdentifier: any }) => el.publicIdentifier
+    ).length;
+  }
+  return { htmlCount, trueCount };
+};
+
+// Listen for messages from extension popup
+chrome.runtime.onMessage.addListener((req, sender, sendRes) => {
+  if (req.type === "CONNECTION_COUNT") {
+    getConnectionCount().then(sendRes);
+  } else if (req.type === "DOWNLOAD") {
+    // todo)) download connections as csv or json or whatever
+  }
+
+  return true;
+});
--- src/popup.ts
+++ src/popup.ts
@@ -1,7 +1,82 @@
 "use strict";
 
+// Import CSS file
 import "./popup.css";
 
-(function () {
-  // todo)) add logic
+/**
+ * Shows the number of connections on the active tab.
+ *
+ * @param {chrome.tabs.Tab} activeTab - The currently active tab in Chrome browser.
+ */
+const showConnectionsCount = (activeTab: chrome.tabs.Tab): void => {
+  if (!activeTab.id) return;
+
+  // Send message to content script to get connection count
+  chrome.tabs.sendMessage(activeTab.id, { type: "CONNECTION_COUNT" }, (res) => {
+    if (!res) return;
+    const { htmlCount, trueCount } = res;
+
+    // Update total element with connection count
+    const total = document.getElementById("total") as HTMLElement;
+    total.innerHTML = `${trueCount} connections`;
+
+    // If HTML count doesn't match true count, add tooltip to explain difference
+    if (htmlCount !== trueCount) {
+      total.innerHTML += `<div id="tooltip">?<span id="tooltip-text">The number of connections shown in the page is ${htmlCount} but the actual number of connections is ${trueCount}.</span></div>`;
+    }
+  });
+};
+
+/**
+ * Shows buttons for user interaction.
+ */
+const showButtons = (): void => {
+  const buttonContainer = document.getElementsByClassName(
+    "button-container"
+  ) as HTMLCollectionOf<HTMLElement>;
+
+  // Display button container element
+  buttonContainer[0].style.display = "block";
+};
+
+const downloadConnections = (): void => {};
+
+// URL for LinkedIn's Connections page
+const connectionPageUrl =
+  "https://www.linkedin.com/mynetwork/invite-connect/connections/";
+
+(async function (): Promise<void> {
+  // Get all tabs that are currently open in current window and are active
+  const tabs: chrome.tabs.Tab[] | undefined = await new Promise((resolve) => {
+    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+      resolve(tabs);
+    });
+  });
+
+  const activeTab: chrome.tabs.Tab | undefined = tabs?.[0];
+
+  if (!activeTab) {
+    return;
+  }
+
+  if (activeTab.url?.includes(connectionPageUrl)) {
+    // Extension is only active on Connections page
+    showButtons();
+    showConnectionsCount(activeTab);
+    // todo)) add event listener to buttons to send message to content script
+    downloadConnections();
+  } else {
+    // If not on Connections page, display message and add click event listener to LinkedIn icon
+    const header = document.getElementById("header") as HTMLElement;
+    header.innerText =
+      "Please open the connections page by clicking the linkedin icon.";
+
+    const svg: any = document.getElementsByTagName("svg")[0];
+
+    svg.addEventListener("click", () => {
+      chrome.tabs.create({
+        url: connectionPageUrl,
+      });
+    });
+  }
 })();
